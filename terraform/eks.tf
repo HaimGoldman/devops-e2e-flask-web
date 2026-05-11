884aed8d034b9d6bdaf4161c41b1d7faa193e206
HaimGoldman <haim.goldman@mail.huji.ac.il>
2026-05-11 08:10:13 +0300

    fix(terraform): correct eks cluster admin policy arn format

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -38,7 +38,7 @@ module "eks" {
       principal_arn = aws_iam_role.github_actions.arn
       policy_associations = {
         admin = {
-          policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterAdminPolicy"
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
           access_scope = { type = "cluster" }
         }
       }
